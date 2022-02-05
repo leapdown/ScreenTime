2e0e909fbfdedbcba7267b1b42da3e2c8ede1d84
leapdown <44557116+leapdown@users.noreply.github.com>
2022-02-05 15:46:45 -0800

    Getting started

---
+++ ActivityMonitor.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Threading;
+
+namespace ScreenTime
+{
+    internal class ActivityMonitor : IDisposable
+    {
+        private IntPtr _keyboardHook;
+        private IntPtr _mouseHook;
+
+        private const Int32 WH_KEYBOARD_LL = 13;
+        private const Int32 WH_MOUSE_LL = 14;
+
+        private bool _disposed;
+        private volatile bool _isActive;
+
+        public ActivityMonitor()
+        {
+            _keyboardHook = WindowsHooks.SetWindowsHookEx(
+                WH_KEYBOARD_LL, OnKeyboardActivity, IntPtr.Zero, 0);
+            _mouseHook = WindowsHooks.SetWindowsHookEx(
+                WH_MOUSE_LL, OnMouseActivity, IntPtr.Zero, 0);
+        }
+
+        public bool IsActive => _isActive;
+
+        public void ResetActive()
+        {
+            _isActive = false;
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                WindowsHooks.UnhookWindowsHookEx(_keyboardHook);
+                WindowsHooks.UnhookWindowsHookEx(_mouseHook);
+
+                _disposed = true;
+            }
+        }
+
+        private IntPtr OnMouseActivity(
+            Int32 Code, IntPtr wParam, IntPtr lParam)
+        {
+            if (Code < 0)
+            {
+                return WindowsHooks.CallNextHookEx(
+                    _mouseHook, Code, wParam, lParam);
+            }
+
+            _isActive = true;
+
+            return WindowsHooks.CallNextHookEx(
+                _mouseHook, Code, wParam, lParam);
+        }
+
+        private IntPtr OnKeyboardActivity(
+            Int32 Code, IntPtr wParam, IntPtr lParam)
+        {
+            if (Code < 0)
+            {
+                return WindowsHooks.CallNextHookEx(
+                    _keyboardHook, Code, wParam, lParam);
+            }
+
+            _isActive = true;
+
+            return WindowsHooks.CallNextHookEx(
+                _keyboardHook, Code, wParam, lParam);
+        }
+
+        ~ActivityMonitor()
+        {
+            Dispose();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Timers;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ScreenTime
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private ActivityMonitor _monitor;
+        private DispatcherTimer _timer;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            _monitor = new ActivityMonitor();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(100);
+            _timer.Tick += CheckActivity;
+            _timer.Start();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+
+        private void CheckActivity(object sender, object e)
+        {
+            text.Text = _monitor.IsActive.ToString();
+            _monitor.ResetActive();
+        }
+    }
+}
